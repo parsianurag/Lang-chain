5d4a2000ed35181bd6979fe81dd458979ca3102a
parsianurag <62787735+parsianurag@users.noreply.github.com>
2024-11-02 21:18:12 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
+
+
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo With Gemma Model")
+input_text=st.text_input("What question you have in mind?")
+
+
+## Ollama Llama2 model
+llm=Ollama(model="gemma:2b")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
+
